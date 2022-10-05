c0dd9e1b790a268080b37edb82c5cd0eca5a4cfe
Tarun Arora <tarun.arora.cse20@iitbhu.ac.in>
2022-10-05 21:19:17 +0530

    test

--- tests/test_sample/TestMediaInfoParser.py
+++ tests/test_sample/TestMediaInfoParser.py
@@ -109,9 +109,8 @@ class TestMediaInfoFetcher(BaseTestCase):
 
     def test__process_track_not_ordereddict(self):
         """Test _process_track method for invalid track."""
-        return
         MOCK_MediaInfoFetcher.reset_mock()
-        track = {}
+        track = {'@type': 'InvalidTrack'}
 
         result = MediaInfoFetcher._process_track(MOCK_MediaInfoFetcher, track)
 
